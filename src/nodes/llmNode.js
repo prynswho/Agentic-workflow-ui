0c2d4b6c3206a2ed938d738233c7bddad38d513c
prynswho <priyanshu27022003@gmail.com>
2026-07-06 23:37:27 +0530

    llm node can take inputs just like the text node(for now we kept is identical)

--- src/nodes/llmNode.js
+++ src/nodes/llmNode.js
@@ -1,20 +1,50 @@
 // llmNode.js
-
+import { useUpdateNodeInternals } from "reactflow";
+import { useEffect } from "react";
+import { useStore } from "../store";
 import { NodeGenerator } from "../components/nodeGenerator";
+import { AutoResizeTextBox } from "../components/fieldRender";
 
+function LLMNode({ id, data }) {
+    const updateNodeInternals = useUpdateNodeInternals();
+    const updateNodeField = useStore((state) => state.updateNodeField);
 
-export const LLMNode = ({ id, data }) => {
-  const fields=[];
-      return (
-          <NodeGenerator
-          title="LLM Node"
-          inputs={[{id: "input1"}, {id: "input2"}]}
-          outputs={[{id: "output1"}]}   
-          accentColor = "#ff008c"
-          >
-              {fields.map(field => (
-              <input key={field.id} type={field.type} placeholder={field.id}/>
-              ))}
-          </NodeGenerator>
-      )
+    const text = data?.text || '';
+
+    const extractValidVars = (text) => {
+        const allValues = new Set();
+        const jsRegex = /\{\{\s*([a-zA-Z_$][a-zA-Z0-9_$]*)\s*\}\}/g;
+        const matched = text.matchAll(jsRegex);
+        for (const matches of matched) {
+            allValues.add(matches[1]);
+        }
+        return Array.from(allValues);
+    };
+
+    const validVariables = extractValidVars(text);
+
+    const input = validVariables.length > 0
+        ? validVariables.map(variable => ({ id: variable }))
+        : [{ id: 'input' }];
+
+    useEffect(() => {
+        updateNodeInternals(id);
+    }, [input.length, updateNodeInternals, id]);
+
+    return (
+        <NodeGenerator
+            title="LLM Node"
+            inputs={input}
+            outputs={[{ id: "output1" }]}
+            accentColor="#de00f2"
+        >
+            <AutoResizeTextBox
+                value={text}
+                onChange={(newValue) => updateNodeField(id, 'text', newValue)}
+                placeholder="text here"
+            />
+        </NodeGenerator>
+    );
 }
+
+export { LLMNode };
